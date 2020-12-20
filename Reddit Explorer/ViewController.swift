7c613558e69469083cc5dc94d0b0bbde1625afe3
Vladimir Aridov <aridov@mail.ru>
2020-12-20 02:33:59 +0200

    Initial Commit

---
+++ Reddit Explorer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reddit Explorer
+//
+//  Created by Vladimir Aridov on 20.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
